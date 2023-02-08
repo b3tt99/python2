59f6f3f10a93dcdc48be24b380f1b3f52f8b04d9
eMobilis <b3tt99@gmail.com>
2023-02-08 10:22:55 +0300

    Python data structures

---
+++ stringlength.py
@@ -0,0 +1,9 @@
+sen = input("Enter a sentence")
+print(len(sen))
+
+numb1 = input("Enter first number")
+numb1 = int(numb1)
+numb2 = input("Enter second number")
+numb2 = int(numb2)
+sum1 = numb1 + numb2
+print(f"The sum is {sum1}")
---
+++ strings.py
@@ -0,0 +1,10 @@
+location = "Nairobi"
+print(location)
+
+age = input("Enter your age")
+age = int(age)
+print(age)
+
+months= age * 12
+print(f"You have lived for {months} months")
+
