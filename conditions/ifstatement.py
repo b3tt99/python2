9024c4a3a7f1229d80f370b110402712af4773fa
eMobilis <b3tt99@gmail.com>
2023-02-09 09:18:54 +0300

    New data structure

---
+++ conditions/ifstatement.py
@@ -0,0 +1,27 @@
+dial = int(input("Enter a Number"))
+if dial == 0 :
+    print("Sh500(2GB+100MINS+500SMS)")
+elif dial == 1 :
+    print("Enrol for bonga points")
+elif dial == 2 :
+    print("Daily Bundles")
+elif dial == 3 :
+    print("Weekly Bundles")
+elif dial == 4 :
+    print("GO MONTHLY")
+elif dial == 5 :
+    print("No Expiry")
+elif dial == 6 :
+    print("Video Bundles")
+elif dial == 7 :
+    print("Okoa Data")
+elif dial == 8 :
+    print("Lipa Mdogo")
+elif dial == 9 :
+    print("Data Plus NEW")
+elif dial == 10 :
+    print("Hot minutes")
+elif dial == 98 :
+    print("MORE")
+else:
+    print("Error")
